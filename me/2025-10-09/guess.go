759a2d4899b8496507b0bb9b9045ed83df84b1de
kimlia.knopf@heidelberg.com <kimlia.knopf@heidelberg.com>
2025-10-09 14:31:39 +0200

    Refactor main function to streamline number input and output

--- me/2025-10-09/guess.go
+++ me/2025-10-09/guess.go
@@ -1,10 +1,23 @@
 package main
 
+import "fmt"	
+
 func main() {
+	guess := Readnumber	()
+	fmt.Println(guess)
+	guess = Readnumber	() // kein : weil gint es schon
+	fmt.Println(guess)
+	guess = Readnumber	()
+	fmt.Println(guess)
 
 }
 
 // Readnumber liefert uns ein int-Zahl
 func Readnumber() int {
-	var n int
+	var n int // alternatin n:=0
+
 	fmt.Print("Bitte gib eine Zahl ein:")
+	fmt.Scan(&n)	
+
+	return n
+}
