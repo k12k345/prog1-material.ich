b3a276f93483558957482fc5a8924cd756b9673d
kimlia.knopf@heidelberg.com <kimlia.knopf@heidelberg.com>
2025-10-09 15:26:17 +0200

    Refactor main function to improve guess input handling and streamline output

--- me/2025-10-09/guess.go
+++ me/2025-10-09/guess.go
@@ -3,21 +3,39 @@ package main
 import "fmt"	
 
 func main() {
-	guess := Readnumber	()
-	fmt.Println(guess)
-	guess = Readnumber	() // kein : weil gint es schon
-	fmt.Println(guess)
-	guess = Readnumber	()
-	fmt.Println(guess)
+	// my_number := 42
+	my_number := rand.Intn(100) +1 
 
+	for i := 0; i < 3; i = i + 1 {
+	guess := ReadNumber	()
+	if NumberGood(guess, my_number) {
+		fmt.Println("Das war richtig!")
+		return // Programm beenden, dann zu else, geht aber ohne else wenn return
+	// } else { //Klammern so
+		fmt.Printf("%d war nicht korrekt!\n", guess) //alt Gr 	
+	}
+
+	fmt.Println("Game Over")
+	}	
 }
 
-// Readnumber liefert uns ein int-Zahl
-func Readnumber() int {
-	var n int // alternatin n:=0
+
+// ReadNumber liefert uns ein int-Zahl
+func ReadNumber() int {
+	var n int // alternativ n:=0
 
 	fmt.Print("Bitte gib eine Zahl ein:")
 	fmt.Scan(&n)	
 
 	return n
+}
+
+// NumberGood prüft ob x eine zufällig gewählte Zahl zwischen 1 und 100 ist 
+func NumberGood (x int, n int) bool {
+	
+	
+	if x == n {
+		return true
+	}	
+	return false
 }
