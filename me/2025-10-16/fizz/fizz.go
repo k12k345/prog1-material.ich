1087bccac6ce4e6d13e9f057b124a010a8176230
kimlia.knopf@heidelberg.com <kimlia.knopf@heidelberg.com>
2025-10-16 14:39:34 +0200

    Implement Fizz function to output numbers from 1 to 30 with FizzBuzz logic and add corresponding example test

---
+++ me/2025-10-16/fizz/fizz.go
@@ -0,0 +1,32 @@
+package fizzbuzz
+
+import "fmt"
+
+//Fizz gibt die Zahlen von 1 bis 30 aus, ersetzt dabei
+//jede durch 3 teilbare Zahl durch "fizz",
+//jede durch 5 teilbare Zahl durch "buzz" und
+//jede durch 3 und 5 teilbare Zahl durch "fizzbuzz"
+
+func Fizz() {
+	for i := 1; i <= 30; i++ {
+
+		// wenn i durch 3 und 5 teilbar ist, gib "fizzbuzz" aus
+		if i%5 == 0 && i%3 == 0 {
+			fmt.Println("fizzbuzz")
+
+		} else if
+		// wenn i durch 3 teilbar ist, gib "fizz" aus
+		i%3 == 0 { //% ist modula, kein rest bei rechnung
+			fmt.Println("fizz")
+		} else if
+		// wenn i durch 5 teilbar ist, gib "buzz" aus
+		i%5 == 0 {
+			fmt.Println("buzz")
+			// fmt.Printf("%10d, %v\n", i, "buzz")
+		} else {
+			fmt.Println(i)
+		}
+		// sonst gib i aus
+	}
+
+}
