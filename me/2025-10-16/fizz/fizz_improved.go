8ccf3c47e873498e8ea3b79da0a77cc3da8f007b
kimlia.knopf@heidelberg.com <kimlia.knopf@heidelberg.com>
2025-10-16 14:51:39 +0200

    Implement FizzImproved function and add example test case

---
+++ me/2025-10-16/fizz/fizz_improved.go
@@ -0,0 +1,25 @@
+package fizzbuzz
+
+import "fmt"
+
+//Fizz gibt die Zahlen von 1 bis n aus, ersetzt dabei
+//jede durch x teilbare Zahl durch "fizz",
+//jede durch y teilbare Zahl durch "buzz" und
+//jede durch x und y teilbare Zahl durch "fizzbuzz"
+
+func FizzImproved(x, y, n int) {
+	for i := 1; i <= n; i++ {
+
+		if i%y == 0 && i%x == 0 {
+			fmt.Println("fizzbuzz")
+		} else if i%x == 0 {
+			fmt.Println("fizz")
+		} else if i%y == 0 {
+			fmt.Println("buzz")
+		} else {
+			fmt.Println(i)
+		}
+
+	}
+
+}
---
+++ me/2025-10-16/fizz/fizz_improved_test.go
@@ -0,0 +1,6 @@
+package fizzbuzz
+
+func ExampleFizzImproved() {
+	FizzImproved(7, 2, 42)
+	// Output:
+}
