e5caa5bcf9046ad335050c633107e108a3f7a02b
kimlia.knopf@heidelberg.com <kimlia.knopf@heidelberg.com>
2025-10-16 13:54:39 +0200

    new

--- me/2025-10-09/datatypes/datatypes_test.go
+++ me/2025-10-09/datatypes/datatypes_test.go
@@ -8,6 +8,7 @@ func Example_integers() {
 
 	fmt.Println(i)
 	fmt.Println(k)
+
 	// Output:
 	// 0
 	// 55
@@ -17,10 +18,13 @@ func Example_various_data_types() {
 	var i1 int
 	i2 := 55
 
+	fmt.Println(i1)
+	fmt.Println(i2)
+
 	var f1 float64
 	f2 := 3.0 / 4.0
 
-	fmt.Prinln(f1)
+	fmt.Println(f1)
 	fmt.Println(f2)
 
 	var b1 bool
@@ -29,20 +33,26 @@ func Example_various_data_types() {
 	fmt.Println(b1)
 	fmt.Println(b2)
 
-	var s1 string //s1 := ""
+	var s1 string // s1 := ""
 	s2 := "Hallo Welt"
 	s3 := s2[4]
+	s4 := s2[2:7]
+	s5 := s4 + s2
 
 	fmt.Println(s1)
 	fmt.Println(s2)
+	fmt.Println(s3)
 	fmt.Printf("%c\n", s3)
+	fmt.Println(s4)
+	fmt.Println(s5)
 
-	//Output:
-	// 0
-	// 55
-	// 0
-	// 0.2
-	// false
-	// true
+	for i := 0; i < len(s2); i++ {
+		fmt.Printf("%c\n", s2[i])
+	}
 
+	for i, c := range s2 {
+		fmt.Printf("%v: %c\n", i, c)
+	}
+
+	// Output:
 }
