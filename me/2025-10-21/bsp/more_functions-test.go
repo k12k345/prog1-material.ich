ea47637f5ebf2b12734ddf6ec7a5f9fa3c1977a7
kimlia.knopf@heidelberg.com <kimlia.knopf@heidelberg.com>
2025-10-21 17:06:42 +0200

    Add examples for more_functions and scopes with output demonstration

---
+++ me/2025-10-21/bsp/more_functions-test.go
@@ -0,0 +1,20 @@
+package varnames
+
+import "fmt"
+
+func Example_more_functions() {
+	PrintSomething(3)
+	// output:
+}
+
+func PrintSomething(x int) {
+	for i := 0; i < 5; i++ {
+		PrintMore(x + i)
+	}
+}
+func PrintMore(x int) {
+	for i := 0; i < x; i++ {
+		fmt.Print(i)
+	}
+	fmt.Println()
+}
---
+++ me/2025-10-21/bsp/scope.go
@@ -0,0 +1,19 @@
+func Example_scopes_loops() {
+	x := 42
+	y := 23
+
+	for x := 0; x < 5; x++ {
+		fmt.Println(x)
+		fmt.Println(y)
+
+	}
+	fmt.Println(x)
+}
+
+//scopes variable verliert gültigkeit bei schleife/funktionen, immer im scope deklarieren
+// variablen kann man neu deklarieren bei scope 
+in vorlesung 2025-10-21 varnames
+zu ihm funtions_test.go
+&&
+scopes_test.go 
+ordner varnames 
